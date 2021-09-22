4bd6c59f52516ef1823cfb46e18ea6cd036a8f7b
Shin Yehjin <68529301+yehjin-00@users.noreply.github.com>
2021-09-22 13:41:12 +0900

    Update 01-큐-10845.py

--- week1-queue-deque/01-큐-10845.py
+++ week1-queue-deque/01-큐-10845.py
@@ -2,65 +2,7 @@
 ## Number: 10845
 ## Date: 2021.09.21
 
-import sys
-
-n = int(sys.stdin.readline())
-queue = []
-
-for i in range(n):
-	order = sys.stdin.readline()
-	if order[:4] == "push": queue.append(order.split()[1])
-
-	elif order == "size": print(len(queue))
-	
-	elif order == "empty":
-		if len(queue) == 0: print(1)
-		else: print(0)
-
-	elif len(queue) == 0: print(-1)
-
-	elif order == "pop": print(queue.pop(0))
-
-	elif order == "front": print(queue[0])
-
-	elif order == "back": print(queue[-1])
-
-
-
-
-import sys
-
-n = int(sys.stdin.readline())
-queue = []
-
-for i in range(n):
-	order = sys.stdin.readline()
-
-	if order == "size": print(len(queue))
-
-	elif order == "empty":
-		if len(queue) == 0: print(1)
-		else: print(0)
-
-	elif len(order.split()) > 1: 
-    	queue.append(order.split()[-1])
-        
-	elif len(queue) == 0: print(-1)
-
-	elif order == "pop": print(queue.pop(0))
-
-	elif order == "front": print(queue[0])
-
-	elif order == "back": print(queue[-1])
-
-
-
-
-
-
-
 # 성공 version
-
 import sys
 
 n = int(sys.stdin.readline())
@@ -84,4 +26,26 @@ for i in range(n):
 
 	elif order[0] == "back": print(queue[-1])
 
+# 왜 안 되는지 모르겠는 ver
+import sys
 
+n = int(sys.stdin.readline())
+queue = []
+
+for i in range(n):
+	order = sys.stdin.readline()
+	if order[:4] == "push": queue.append(order.split()[1])
+
+	elif order == "size": print(len(queue))
+	
+	elif order == "empty":
+		if len(queue) == 0: print(1)
+		else: print(0)
+
+	elif len(queue) == 0: print(-1)
+
+	elif order == "pop": print(queue.pop(0))
+
+	elif order == "front": print(queue[0])
+
+	elif order == "back": print(queue[-1])
