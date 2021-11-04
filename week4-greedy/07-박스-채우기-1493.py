67d05350e757f489882f6f3d9ec63d538ed3c3fa
Shin Yehjin <68529301+yehjin-00@users.noreply.github.com>
2021-11-04 23:14:19 +0900

    Update 07-박스-채우기-1493.py

--- week4-greedy/07-박스-채우기-1493.py
+++ week4-greedy/07-박스-채우기-1493.py
@@ -1,3 +1,20 @@
-## Title: 
-## Number: 
-## Date: 2021.10.2
+## Title: 박스 채우기
+## Number: 1493
+## Date: 2021.11.03
+
+L, W, H = map(int, input().split())
+N = int(input())
+
+cube = dict()
+for i in range(N):
+	a, b = map(int, input().split())
+	cube[a] = b
+
+total = 0
+for n in list(cube):
+	total += 2^(n*3) * cube[n]
+
+if total < L*W*H:
+	print(-1)
+else:
+	
